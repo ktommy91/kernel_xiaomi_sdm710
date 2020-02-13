a6a8367594beb50405de6a56b45d385b70063200
mawrick26 <mawrick26@gmail.com>
2020-02-13 21:06:34 +0100

    PM / s2idle: Fix missed d14367b3f04c03809f017f6403bc9db5116ee6d0
    Signed-off-by: mawrick26 <mawrick26@gmail.com>

--- kernel/time/tick-common.c
+++ kernel/time/tick-common.c
@@ -517,8 +517,9 @@ void tick_unfreeze(void)
 	if (tick_freeze_depth == num_online_cpus()) {
 		timekeeping_resume();
 		sched_clock_resume();
-		//trace_suspend_resume(TPS("timekeeping_freeze"),
-		//		     smp_processor_id(), false);
+		system_state = SYSTEM_RUNNING;
+//		trace_suspend_resume(TPS("timekeeping_freeze"),
+//				     smp_processor_id(), false);
 	} else {
 		tick_resume_local();
 	}
